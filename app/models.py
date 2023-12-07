2e47121b076f0e54d4ea1d22dc1c19bee2abfeca
Egor Ivanov <101597786+Ozondo@users.noreply.github.com>
2023-12-07 18:25:34 +0300

    final

---
+++ app/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class TagManager(models.Manager):
+    def most_popular(self, total):
+        return self.all()[:total]
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    objects = TagManager()
+
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.user.username
+
+class QuestionsManager(models.Manager):
+        def newest(self):
+            return [{'question': q, 'tags': self.get_tags(q.pk)} for q in self.order_by('-created_at')]
+    
+        def best(self):
+            return [{'question': q, 'tags': self.get_tags(q.pk)} for q in self.order_by('-updated_at')]
+    
+        def with_tag(self, tag_name):
+            return [{'question': q, 'tags': self.get_tags(q.pk)} for q in self.filter(tags=tag_name)]
+    
+        def with_id(self, id):
+            q = self.get(pk=id)
+            return {'question': q, 'tags': self.get_tags(q.pk)}
+    
+        def get_tags(self, id):
+            tag_names = self.values_list('tags', flat=True).filter(pk=id)
+            return Tag.objects.filter(pk__in=tag_names)  
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = QuestionsManager()
+
+    def __str__(self):
+        return self.title
+
+class AnswerManager(models.Manager):
+        def newest(self, question_id):
+            return self.filter(question=question_id).order_by('-created_at')
+
+        def best(self, question_id):
+            return self.filter(question=question_id).order_by( '-updated_at')
+
+class Answer(models.Model):
+    content = models.TextField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = AnswerManager()
+
+    def __str__(self):
+        return f"Answer to '{self.question.title}'"
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} likes {self.question.title}{' - ' + self.answer.content if self.answer else ''}"
+
---
+++ app/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator
+from app.models import Question, Answer, Tag
+from random import randint
+
+# Create your views here.
+
+def paginate(objects, request, per_page=5):
+    page = str(request.GET.get('page', 1))
+    page = int(page) if page.isdigit() else 1
+
+    paginator = Paginator(objects, per_page)
+    page = 1 if page < 0 else paginator.num_pages if page > paginator.num_pages else page
+    return {'page_item': paginator.page(page), 'pages_range': paginator.get_elided_page_range(page, on_each_side=2)}
+
+# New
+def index(request):
+    questions = Question.objects.newest()
+    tags = Tag.objects.most_popular(10)
+    return render(request, 'index.html', {'page': paginate(questions, request), 'tags': tags, 'is_logged_in': True, 'component_to_paginate': 'components/question.html'})
+
+# Hot
+def hot_questions(request):
+    questions = Question.objects.best()
+    tags = Tag.objects.most_popular(10)
+    return render(request, 'index_hot.html', {'tags': tags, 'page': paginate(questions, request), 'component_to_paginate': 'components/question.html'})
+
+# Tag
+def tag(request, tag_name):
+    try:
+        tag_item = Tag.objects.get(pk=tag_name)
+    except Tag.DoesNotExist:
+        tag_item = Tag.objects.all()[0]
+    questions = Question.objects.with_tag(tag_item.tag_name)
+    tags = Tag.objects.most_popular(10)
+    return render(request, 'index_tags.html', {'tag_item': tag_item, 'tags': tags, 'page': paginate(questions, request), 'component_to_paginate': 'components/question.html'})
+
+# Question
+def question(request, question_id):
+    question_item = Question.objects.with_id(question_id)
+    answers = Answer.objects.best(question_id)
+    tags = Tag.objects.most_popular(10)
+    return render(request, 'question.html', {'question': question_item, 'tags': tags, 'page': paginate(answers, request), 'component_to_paginate': 'components/answer.html'})
+
+# Log In
+def login(request):
+    tags = Tag.objects.most_popular(10)
+    return render(request, 'login.html', {'tags': tags})
+
+# Sign Up
+def signup(request):
+    tags = Tag.objects.most_popular(10)
+    return render(request, 'signup.html', {'tags': tags})
+
+# Ask question
+def ask(request):
+    title = request.GET.get('new_title', 'New title')
+    tags = Tag.objects.most_popular(10)
+    return render(request, 'ask.html', {'tags': tags, 'title': title})
+
+# User settings
+def settings(request):
+    tags = Tag.objects.most_popular(10)
+    return render(request, 'settings.html', {'tags': tags, 'is_logged_in': True})
